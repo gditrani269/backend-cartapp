ed6f919c39ff74ab7a2042a30592522a518567ed
unknown <gditrani269@gmail.com>
2024-05-09 18:36:59 -0300

    ajustes menores y CORS

--- src/main/java/com/andres/backend/cartapp/backendcartapp/controllers/ProductController.java
+++ src/main/java/com/andres/backend/cartapp/backendcartapp/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.andres.backend.cartapp.backendcartapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,8 @@ import com.andres.backend.cartapp.backendcartapp.models.entities.Product;
 import com.andres.backend.cartapp.backendcartapp.services.ProductService;
 
 @RestController
+//para solucionar el problema de CORS CrossOrigin
+@CrossOrigin (origins = "http://localhost:8082")
 public class ProductController {
 
     @Autowired
